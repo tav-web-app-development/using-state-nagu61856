5e561184e71d765dcca662e744fcaa47bb8ce119
charan0777 <charanchinna0007@gmail.com>
2024-03-05 18:54:53 -0500

    my code

--- src/components/ProductCardContainer.jsx
+++ src/components/ProductCardContainer.jsx
@@ -1,4 +1,5 @@
-import ProductCard from "./ProductCard";
+import ProductCard from "./ProductCard.jsx";
+
 
 export default function ProductCardContainer({ products }) {
   const productsJsx = products.map((product) => (
